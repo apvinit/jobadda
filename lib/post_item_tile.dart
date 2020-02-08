1a2e8329b71a92eeac548e94759f5853faad4572
h4rdw1r3 <vinaugust6@gmail.com>
2020-02-08 19:40:20 +0530

    add click listener for going to detail page

--- lib/post_item_tile.dart
+++ lib/post_item_tile.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:jobadda/post_detail_page.dart';
 
 class PostItemTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: () {},
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (_) => PostDetailPage(), fullscreenDialog: true));
+      },
       child: Card(
         elevation: 0.0,
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Text('UPSSC 2019 Results', style: TextStyle(fontSize: 16.0),),
+          child: Text(
+            'UPSSC 2019 Results',
+            style: TextStyle(fontSize: 16.0),
+          ),
         ),
       ),
     );

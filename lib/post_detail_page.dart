c0c52213864c455c391f85b6e0ac770e4479ddcd
h4rdw1r3 <vinaugust6@gmail.com>
2020-02-08 19:49:53 +0530

    create placeholder  post detail page

---
+++ lib/post_detail_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:jobadda/posts/post_dates.dart';
+import 'package:jobadda/posts/post_info.dart';
+import 'package:jobadda/posts/post_links.dart';
+
+class PostDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Post Detail',
+          ),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tooltip(
+                  message: 'Post Info',
+                  preferBelow: false,
+                  child: Tab(icon: Icon(Icons.info))),
+              Tooltip(
+                  preferBelow: false,
+                  message: 'Important Dates',
+                  child: Tab(icon: Icon(Icons.date_range))),
+              Tooltip(
+                  preferBelow: false,
+                  message: 'Important Links',
+                  child: Tab(icon: Icon(Icons.link)))
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[PostInfo(), PostDates(), PostLinks()],
+        ),
+      ),
+    );
+  }
+}

60729f108151370d4f9d52488f7c5a9773f416a2
h4rdw1r3 <vinaugust6@gmail.com>
2020-02-09 21:21:41 +0530

    add methods for getting recent posts relatively

---
+++ lib/services/database.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:jobadda/model/post.dart';
+
+class Database {
+  var _postsCollection = Firestore.instance.collection('jposts');
+
+  List<Post> _postListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.documents.map((doc) => Post.fromMap(doc.data)).toList();
+  }
+
+  Future<List<Post>> getRecentResults() async {
+    var snapshot = await _postsCollection
+        .where('type', isEqualTo: 'Results')
+        .limit(3)
+        .getDocuments();
+    return _postListFromSnapshot(snapshot);
+  }
+
+  Future<List<Post>> getRecentAdmitCards() async {
+    var snapshot = await _postsCollection
+        .where('type', isEqualTo: 'Admit Card')
+        .limit(3)
+        .getDocuments();
+    return _postListFromSnapshot(snapshot);
+  }
+
+  Future<List<Post>> getRecentJobs() async {
+    var snapshot = await _postsCollection
+        .where('type', isEqualTo: 'Latest Job')
+        .limit(3)
+        .getDocuments();
+    return _postListFromSnapshot(snapshot);
+  }
+
+  Future<List<Post>> getRecentAnskwerKeys() async {
+    var snapshot = await _postsCollection
+        .where('type', isEqualTo: 'Answer Keys')
+        .limit(3)
+        .getDocuments();
+    return _postListFromSnapshot(snapshot);
+  }
+
+  Future<List<Post>> getRecentAdmissions() async {
+    var snapshot = await _postsCollection
+        .where('type', isEqualTo: 'Admission')
+        .limit(3)
+        .getDocuments();
+    return _postListFromSnapshot(snapshot);
+  }
+
+  Future<List<Post>> getRecentSyllabus() async {
+    var snapshot = await _postsCollection
+        .where('type', isEqualTo: 'Syllabus')
+        .limit(3)
+        .getDocuments();
+    return _postListFromSnapshot(snapshot);
+  }
+}

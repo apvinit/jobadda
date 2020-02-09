a5574e8ac8f1f31b4c412bbefc4ce94d0f96049f
h4rdw1r3 <vinaugust6@gmail.com>
2020-02-09 23:00:51 +0530

    show post links and when click open chrome custom tab

--- lib/posts/post_link_tile.dart
+++ lib/posts/post_link_tile.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
+import 'package:jobadda/model/important_link.dart';
 
 class PostLinkTile extends StatelessWidget {
+  final ImportantLink link;
+  PostLinkTile(this.link);
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 0.0,
       child: InkWell(
-        onTap: () {},
+        onTap: () => _launchURL(context, link.url),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Text(
-            'Download this thing',
+            link.title,
             style: TextStyle(
                 fontSize: 16.0,
                 color: Colors.blue,
@@ -19,5 +23,29 @@ class PostLinkTile extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _launchURL(BuildContext context, String url) async {
+    try {
+      await launch(
+        url,
+        option: new CustomTabsOption(
+          toolbarColor: Theme.of(context).primaryColor,
+          enableDefaultShare: false,
+          enableUrlBarHiding: true,
+          showPageTitle: true,
+          animation: new CustomTabsAnimation.slideIn(),
+          extraCustomTabs: <String>[
+            // ref. https://play.google.com/store/apps/details?id=org.mozilla.firefox
+            'org.mozilla.firefox',
+            // ref. https://play.google.com/store/apps/details?id=com.microsoft.emmx
+            'com.microsoft.emmx',
+          ],
+        ),
+      );
+    } catch (e) {
+      // An exception is thrown if browser app is not installed on Android device.
+      debugPrint(e.toString());
+    }
   }
 }
--- lib/posts/post_links.dart
+++ lib/posts/post_links.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:jobadda/model/post.dart';
 import 'package:jobadda/posts/post_link_tile.dart';
 
 class PostLinks extends StatelessWidget {
+  @required
+  final Post post;
+
+  PostLinks({this.post});
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 30, itemBuilder: (context, index) => PostLinkTile());
+      itemCount: post.links.length,
+      itemBuilder: (context, index) => PostLinkTile(post.links[index]),
+    );
   }
 }

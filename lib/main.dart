b8f342b55eb9d8be1238f1ee96b52fb6de2b46e5
h4rdw1r3 <vinaugust6@gmail.com>
2020-02-25 15:19:57 +0530

    use custom primary secondary color

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,13 @@ import 'package:jobadda/home_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Job Adda',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xFF1A535C),
+        accentColor: Color(0xFFFF735E),
       ),
       home: HomePage(),
     );

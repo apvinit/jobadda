0fbe558cbc69d2aab4a268f7fb15d5ec29a2aa77
h4rdw1r3 <vinaugust6@gmail.com>
2020-02-25 23:25:23 +0530

    create overline all caps and size to 12

--- lib/posts/post_info.dart
+++ lib/posts/post_info.dart
@@ -9,95 +9,108 @@ class PostInfo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Text('Post', style: TextStyle(color: Theme.of(context).accentColor)),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              post.name,
-              style: TextStyle(fontSize: 18.0),
-            ),
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Posted On',
-                        style: TextStyle(color: Theme.of(context).accentColor)),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Text(
-                        post.createdDate,
-                        style: TextStyle(fontSize: 18.0),
-                      ),
-                    ),
-                  ],
-                ),
+    return SingleChildScrollView(
+      child: Container(
+        color: Colors.white54,
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text('POST',
+                style: TextStyle(
+                    fontSize: 12.0, color: Theme.of(context).accentColor)),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                post.name,
+                style: TextStyle(fontSize: 20.0),
               ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Updated On',
-                        style: TextStyle(color: Theme.of(context).accentColor)),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8.0),
-                      child: Text(
-                        post.updatedDate,
-                        style: TextStyle(fontSize: 18.0),
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('POSTED ON',
+                          style: TextStyle(
+                              fontSize: 12.0,
+                              color: Theme.of(context).accentColor)),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text(
+                          post.createdDate,
+                          style: TextStyle(fontSize: 20.0),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text('UPDATED ON',
+                          style: TextStyle(
+                              fontSize: 12.0,
+                              color: Theme.of(context).accentColor)),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Text(
+                          post.updatedDate,
+                          style: TextStyle(fontSize: 20.0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Text('ORGANISATION',
+                style: TextStyle(
+                    fontSize: 12.0, color: Theme.of(context).accentColor)),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                post.organisation,
+                style: TextStyle(fontSize: 20.0),
               ),
-            ],
-          ),
-          Text('Organisation',
-              style: TextStyle(color: Theme.of(context).accentColor)),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              post.organisation,
-              style: TextStyle(fontSize: 18.0),
             ),
-          ),
-          Text('Short Info',
-              style: TextStyle(color: Theme.of(context).accentColor)),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              post.info,
-              style: TextStyle(fontSize: 18.0),
+            Text('SHORT INFO',
+                style: TextStyle(
+                    fontSize: 12.0, color: Theme.of(context).accentColor)),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                post.info,
+                style: TextStyle(fontSize: 20.0),
+              ),
             ),
-          ),
-          Text('Total Post Vacancy',
-              style: TextStyle(color: Theme.of(context).accentColor)),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              post.totalVacancy != null
-                  ? post.totalVacancy.toString()
-                  : 'To be updated',
-              style: TextStyle(fontSize: 18.0),
+            Text('TOTAL POST VACANCY',
+                style: TextStyle(
+                    fontSize: 12.0, color: Theme.of(context).accentColor)),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                post.totalVacancy != null
+                    ? post.totalVacancy.toString()
+                    : 'To be updated',
+                style: TextStyle(fontSize: 20.0),
+              ),
             ),
-          ),
-          if (post.fees.length != 0)
-            Text('Application Fees',
-                style: TextStyle(color: Theme.of(context).accentColor)),
-          SizedBox(height: 8.0),
-          for (var fee in post.fees)
-            Text(
-              '${fee.title} - ${fee.amount}',
-              style: TextStyle(fontSize: 18.0),
-            ),
-          SizedBox(height: 8.0),
-        ],
+            if (post.fees.length != 0)
+              Text('APPLICATION FEES',
+                  style: TextStyle(
+                      fontSize: 12.0, color: Theme.of(context).accentColor)),
+            SizedBox(height: 8.0),
+            for (var fee in post.fees)
+              Text(
+                '${fee.title} - ${fee.amount}',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            SizedBox(height: 8.0),
+          ],
+        ),
       ),
     );
   }

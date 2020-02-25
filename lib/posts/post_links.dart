870bc68b5a459cba67b4214705454b6b6089368f
h4rdw1r3 <vinaugust6@gmail.com>
2020-02-25 16:41:01 +0530

    show empty state when no image

--- lib/posts/post_links.dart
+++ lib/posts/post_links.dart
@@ -9,9 +9,18 @@ class PostLinks extends StatelessWidget {
   PostLinks({this.post});
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: post.links.length,
-      itemBuilder: (context, index) => PostLinkTile(post.links[index]),
-    );
+    if (post.links.length == 0) {
+      return Container(
+        padding: EdgeInsets.only(top: 150),
+        color: Colors.white,
+        alignment: Alignment.topCenter,
+        child: Image.asset('images/empty.png', height: 200),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: post.links.length,
+        itemBuilder: (context, index) => PostLinkTile(post.links[index]),
+      );
+    }
   }
 }

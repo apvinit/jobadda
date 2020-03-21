615fa5e3e215fe6f69aecf1fcbf94c7b37fdd1a6
h4rdw1r3 <vinaugust6@gmail.com>
2020-03-21 13:53:22 +0530

    pass interstial object to the post items

--- lib/post_list_page.dart
+++ lib/post_list_page.dart
@@ -1,3 +1,4 @@
+import 'package:admob_flutter/admob_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:jobadda/model/post.dart';
 import 'package:jobadda/post_item_tile.dart';
@@ -5,17 +6,42 @@ import 'package:jobadda/services/database.dart';
 
 import 'posts/post_tile_shimmer.dart';
 
-class PostListPage extends StatelessWidget {
+class PostListPage extends StatefulWidget {
   @required
   final String title;
 
   PostListPage({this.title});
 
   @override
+  _PostListPageState createState() => _PostListPageState();
+}
+
+class _PostListPageState extends State<PostListPage> {
+  AdmobInterstitial _interstitial;
+
+  @override
+  void initState() {
+    super.initState();
+    _interstitial = AdmobInterstitial(
+      adUnitId: 'ca-app-pub-3940256099942544/4411468910',
+      listener: (AdmobAdEvent event, Map<String, dynamic> args) {
+        if (event == AdmobAdEvent.closed) _interstitial.load();
+      },
+    );
+    _interstitial.load();
+  }
+
+  @override
+  void dispose() {
+    _interstitial.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     var db = Database();
     var future;
-    switch (title) {
+    switch (widget.title) {
       case 'Results':
         future = db.getResults();
         break;
@@ -37,22 +63,24 @@ class PostListPage extends StatelessWidget {
     }
 
     return Scaffold(
-        appBar: AppBar(title: Text(title)),
-        body: FutureBuilder(
-          future: future,
-          builder: (context, AsyncSnapshot<List<Post>> snapshot) {
-            if (snapshot.hasData) {
-              var posts = snapshot.data;
-              return ListView.builder(
-                  itemCount: posts.length,
-                  itemBuilder: (context, index) {
-                    return PostItemTile(post: posts[index]);
-                  });
-            } else {
-              return ListView.builder(
-                  itemCount: 15, itemBuilder: (_, __) => PostTileShimmer());
-            }
-          },
-        ));
+      appBar: AppBar(title: Text(widget.title)),
+      body: FutureBuilder(
+        future: future,
+        builder: (context, AsyncSnapshot<List<Post>> snapshot) {
+          if (snapshot.hasData) {
+            var posts = snapshot.data;
+            return ListView.builder(
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  return PostItemTile(
+                      post: posts[index], interstitial: _interstitial);
+                });
+          } else {
+            return ListView.builder(
+                itemCount: 15, itemBuilder: (_, __) => PostTileShimmer());
+          }
+        },
+      ),
+    );
   }
 }

89595db58e7bc8a1a0910c2e23f6bb1497be0c95
h4rdw1r3 <vinaugust6@gmail.com>
2020-04-14 01:49:09 +0530

    create placeholder widgets for age limit and vacancy

--- lib/posts/post_vacancy_detail.dart
+++ lib/posts/post_vacancy_detail.dart
@@ -15,57 +15,173 @@ class PostVacancyDetail extends StatelessWidget {
 
     var bodyStyle = TextStyle(fontSize: 18.0);
 
-    return Container(
-      margin: EdgeInsets.all(8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          if (post.totalVacancy != null) ...[
-            Text('TOTAL POST VACANCY', style: headingStyle),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 8.0),
-              child: Text(
-                post.totalVacancy.toString(),
-                style: bodyStyle,
-              ),
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.all(8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            if (post.fees.length != 0) ...[
+              Text('APPLICATION FEES', style: headingStyle),
+              SizedBox(height: 8.0),
+              for (var fee in post.fees)
+                Container(
+                  margin: EdgeInsets.only(bottom: 4),
+                  padding: EdgeInsets.symmetric(vertical: 4),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(
+                              width: 0.35, color: Colors.grey.shade300))),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 5,
+                        child: Text(
+                          fee.title,
+                          style: bodyStyle,
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          '₹${fee.amount}',
+                          style: bodyStyle.copyWith(
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).primaryColor),
+                          textAlign: TextAlign.center,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+            ],
+            Row(
+              children: <Widget>[
+                Text('AGE LIMIT', style: headingStyle),
+                Expanded(
+                  child: Text(
+                    '(as on 06 Jun 2019)',
+                    textAlign: TextAlign.end,
+                    style: Theme.of(context).textTheme.overline.copyWith(
+                        fontSize: 14, color: Theme.of(context).accentColor),
+                  ),
+                ),
+              ],
             ),
-            Divider(),
-          ],
-          if (post.fees.length != 0) ...[
-            Text('APPLICATION FEES', style: headingStyle),
-            SizedBox(height: 8.0),
-            for (var fee in post.fees)
+            for (var d in data)
               Container(
                 margin: EdgeInsets.only(bottom: 4),
                 padding: EdgeInsets.symmetric(vertical: 4),
                 decoration: BoxDecoration(
                     border: Border(
-                        bottom:
-                            BorderSide(width: 0.35, color: Colors.grey.shade300))),
+                        bottom: BorderSide(
+                            width: 0.35, color: Colors.grey.shade300))),
                 child: Row(
                   children: <Widget>[
                     Expanded(
-                      flex: 5,
+                      flex: 3,
                       child: Text(
-                        fee.title,
+                        d[0],
                         style: bodyStyle,
                       ),
                     ),
                     Expanded(
+                      flex: 2,
                       child: Text(
-                        fee.amount,
+                        d[1],
                         style: bodyStyle.copyWith(
                             fontWeight: FontWeight.bold,
                             color: Theme.of(context).primaryColor),
-                        textAlign: TextAlign.center,
+                        textAlign: TextAlign.end,
                       ),
                     )
                   ],
                 ),
               ),
-          ]
+            if (post.totalVacancy != null) ...[
+              SizedBox(height: 8),
+              Text('TOTAL POST VACANCY', style: headingStyle),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8.0),
+                child: Text(
+                  post.totalVacancy.toString(),
+                  style: bodyStyle,
+                ),
+              ),
+              Divider(),
+            ],
+            Text('VACANCY DETAILS', style: headingStyle),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Column(
+                children: <Widget>[
+                  VacancyTile(),
+                  VacancyTile(),
+                  VacancyTile(),
+                  VacancyTile(),
+                  VacancyTile(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class VacancyTile extends StatefulWidget {
+  const VacancyTile({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _VacancyTileState createState() => _VacancyTileState();
+}
+
+class _VacancyTileState extends State<VacancyTile> {
+  bool isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          InkWell(
+            onTap: () {
+              setState(() {
+                isExpanded = !isExpanded;
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    'Header',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  if (isExpanded)
+                    Icon(Icons.keyboard_arrow_up)
+                  else
+                    Icon(Icons.keyboard_arrow_down)
+                ],
+              ),
+            ),
+          ),
+          if (isExpanded) Text('Body'),
         ],
       ),
     );
   }
 }
+
+const data = [
+  ["Minimum", "17 Years"],
+  ["Maximum", "42 Years"],
+  ["Category No. 24, 27 37", "18 - 42 Years"],
+  ["Consultant - Applied Mathematics", "30-40 Years"],
+  ["Project Administrator", "25-30 Years"]
+];

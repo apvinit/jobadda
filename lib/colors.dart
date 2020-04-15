dac8c428d8c5daa9709b49be83d38d44719bbb0c
h4rdw1r3 <vinaugust6@gmail.com>
2020-04-16 04:47:23 +0530

    refactor and change color orders

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
+/// Order of the category item for the colors
+/// Results = 0
+/// AdmitCard = 1
+/// Latest Job = 2
+/// Answer Keys = 3
+/// Admission = 4
+/// Syllabus = 5
 final colors = [
-    // Results = 0
-    // AdmitCard = 1
-    // Latest Job = 2
-    // Answer Keys = 3
-    // Admission = 4
-    // Syllabus = 5
-    Colors.indigo,
-    Colors.green,
-    Colors.purple,
-    Colors.red,
-    Colors.orange,
-    Colors.teal,
-  ];
+  Colors.indigo,
+  Colors.green[800],
+  Colors.purple,
+  Colors.red,
+  Colors.teal,
+  Colors.orange,
+];

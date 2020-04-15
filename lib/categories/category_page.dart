e25bbd653eec7815bc6fde756bbf9c80977da7c3
h4rdw1r3 <vinaugust6@gmail.com>
2020-04-16 04:34:59 +0530

    wrap page item in card for nice white background

--- lib/categories/category_page.dart
+++ lib/categories/category_page.dart
@@ -7,33 +7,36 @@ class CatergoyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget getItem(String title, int c) {
-      return InkWell(
-        onTap: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (_) => PostListPage(title: title)));
-        },
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border(left: BorderSide(color: colors[c], width: 6)),
-          ),
-          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                title.toUpperCase(),
-                style: TextStyle(
-                  color: colors[c],
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
+      return Card(
+        elevation: 0,
+        child: InkWell(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (_) => PostListPage(title: title)));
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border(left: BorderSide(color: colors[c], width: 6)),
+            ),
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  title.toUpperCase(),
+                  style: TextStyle(
+                    color: colors[c],
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              Icon(
-                Icons.keyboard_arrow_right,
-                color: colors[c],
-              ),
-            ],
+                Icon(
+                  Icons.keyboard_arrow_right,
+                  color: colors[c],
+                ),
+              ],
+            ),
           ),
         ),
       );

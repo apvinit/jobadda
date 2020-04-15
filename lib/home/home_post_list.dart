e9173281a456788bb751b3d4ed179e2a81bec21a
h4rdw1r3 <vinaugust6@gmail.com>
2020-04-15 15:59:19 +0530

    create home post list and post tile widget

---
+++ lib/home/home_post_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:jobadda/model/post.dart';
+import 'package:jobadda/posts/post_tile_shimmer.dart';
+import 'package:jobadda/services/database.dart';
+
+import 'home_post_list_tile.dart';
+
+
+class HomePostList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Database().getRecentPosts(),
+      builder: (context, AsyncSnapshot<List<Post>> snapshot) {
+        if (snapshot.hasData) {
+          var posts = snapshot.data;
+          return ListView(
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: posts.map((post) => HomePostListTile(post)).toList());
+        } else {
+          return Column(
+            children: <Widget>[
+              PostTileShimmer(),
+              PostTileShimmer(),
+              PostTileShimmer(),
+            ],
+          );
+        }
+      },
+    );
+  }
+}
---
+++ lib/home/home_post_list_tile.dart
@@ -0,0 +1,107 @@
+import 'dart:math';
+
+import 'package:admob_flutter/admob_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:jobadda/model/post.dart';
+
+import '../post_detail_page.dart';
+
+class HomePostListTile extends StatelessWidget {
+  final Post post;
+  final AdmobInterstitial interstitial;
+
+  final colors = [
+    // Results = 0
+    // AdmitCard = 1
+    // Latest Job = 2
+    // Answer Keys = 3
+    // Admission = 4
+    // Syllabus = 5
+    Colors.indigo,
+    Colors.green,
+    Colors.purple,
+    Colors.red,
+    Colors.orange,
+    Colors.teal,
+  ];
+
+  final random = Random();
+
+  HomePostListTile(this.post, {this.interstitial});
+  @override
+  Widget build(BuildContext context) {
+    var color;
+
+    switch (post.type) {
+      case "Results":
+        color = colors[0];
+        break;
+      case "Admit Card":
+        color = colors[1];
+        break;
+      case "Latest Job":
+        color = colors[2];
+        break;
+      case "Answer Keys":
+        color = colors[3];
+        break;
+
+      case "Admission":
+        color = colors[4];
+        break;
+      case "Syllabus":
+        color = colors[5];
+        break;
+    }
+
+    return Card(
+      elevation: 0,
+      child: InkWell(
+        onTap: () async {
+          if (await interstitial?.isLoaded ?? false) {
+            interstitial.show();
+            navigateToPostDetail(context);
+          } else
+            navigateToPostDetail(context);
+        },
+        child: Container(
+          padding: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+              border: Border(left: BorderSide(color: color, width: 6))),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    post.type.toUpperCase(),
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle2
+                        .copyWith(color: color),
+                  ),
+                  Text(post.createdDate,
+                      style: Theme.of(context)
+                          .textTheme
+                          .caption
+                          .copyWith(fontSize: 14)),
+                ],
+              ),
+              SizedBox(height: 8),
+              Text(post.title, style: Theme.of(context).textTheme.subtitle1)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void navigateToPostDetail(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (_) => PostDetailPage(post: post), fullscreenDialog: true),
+    );
+  }
+}

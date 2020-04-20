57e25febfe7b71a22df024f137a139c9c1d15533
h4rdw1r3 <vinaugust6@gmail.com>
2020-04-20 18:46:21 +0530

    get single post and display

--- lib/post_detail_page.dart
+++ lib/post_detail_page.dart
@@ -4,80 +4,65 @@ import 'package:jobadda/posts/post_dates.dart';
 import 'package:jobadda/posts/post_info.dart';
 import 'package:jobadda/posts/post_links.dart';
 import 'package:jobadda/posts/post_vacancy_detail.dart';
-
-import 'colors.dart';
+import 'package:jobadda/services/remote_api.dart';
 
 class PostDetailPage extends StatelessWidget {
   @required
-  final Post post;
+  final String id;
+  final Color color;
 
-  PostDetailPage({this.post});
+  PostDetailPage({this.id, this.color});
   @override
   Widget build(BuildContext context) {
-    var color;
-
-    switch (post.type) {
-      case "Results":
-        color = colors[0];
-        break;
-      case "Admit Card":
-        color = colors[1];
-        break;
-      case "Latest Job":
-        color = colors[2];
-        break;
-      case "Answer Keys":
-        color = colors[3];
-        break;
-
-      case "Admission":
-        color = colors[4];
-        break;
-      case "Syllabus":
-        color = colors[5];
-        break;
-    }
-
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: Text(post.title),
-        ),
-        body: Column(
-          children: <Widget>[
-            Card(
-              margin: EdgeInsets.all(0.0),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-              elevation: 2,
-              child: TabBar(
-                labelColor: color,
-                indicatorColor: color,
-                tabs: <Widget>[
-                  Tab(text: 'INFO'),
-                  Tab(text: 'DETAIL'),
-                  Tab(text: 'DATES'),
-                  Tab(text: 'LINKS')
-                ],
+    return FutureBuilder(
+      future: getPostById(id),
+      builder: (context, AsyncSnapshot<Post> snapshot) {
+        if (snapshot.hasData) {
+          var post = snapshot.data;
+          return DefaultTabController(
+            length: 4,
+            child: Scaffold(
+              appBar: AppBar(
+                elevation: 0,
+                title: Text(post.title),
               ),
-            ),
-            Expanded(
-              child: TabBarView(
+              body: Column(
                 children: <Widget>[
-                  PostInfo(post: post, color: color),
-                  PostVacancyDetail(
-                    post: post,
-                    color: color
+                  Card(
+                    margin: EdgeInsets.all(0.0),
+                    shape:
+                        RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+                    elevation: 2,
+                    child: TabBar(
+                      labelColor: color,
+                      indicatorColor: color,
+                      tabs: <Widget>[
+                        Tab(text: 'INFO'),
+                        Tab(text: 'DETAIL'),
+                        Tab(text: 'DATES'),
+                        Tab(text: 'LINKS')
+                      ],
+                    ),
                   ),
-                  PostDates(post: post),
-                  PostLinks(post: post)
+                  Expanded(
+                    child: TabBarView(
+                      children: <Widget>[
+                        PostInfo(post: post, color: color),
+                        PostVacancyDetail(post: post, color: color),
+                        PostDates(post: post),
+                        PostLinks(post: post)
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
-          ],
-        ),
-      ),
+          );
+        }
+        // by default show post detail shimmer
+        // for now use circular progress indicator
+        return CircularProgressIndicator();
+      },
     );
   }
 }

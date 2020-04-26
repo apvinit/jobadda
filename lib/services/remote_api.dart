316d958cb3298e7d2541745eaeee52c641581d6b
h4rdw1r3 <vinaugust6@gmail.com>
2020-04-26 19:35:15 +0530

    use domain name instead of ip address

--- lib/services/remote_api.dart
+++ lib/services/remote_api.dart
@@ -7,9 +7,9 @@ import 'package:http/http.dart' as http;
 
 String getBaseUrl() {
   if (kReleaseMode) {
-    return "http://192.168.43.7:1323/api";
+    return "http://jobadda.company:1323/api";
   }
-  return "http://192.168.43.7:1323/api";
+  return "http://jobadda.company:1323/api";
 }
 
 List<PostShortInfo> parsePosts(String responseBody) {

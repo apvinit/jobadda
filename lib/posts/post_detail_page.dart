9f6a9c0f3a479979c5d62887d0c8100ea9fa91f9
h4rdw1r3 <vinaugust6@gmail.com>
2020-04-28 03:22:17 +0530

    create and share deep link

--- lib/posts/post_detail_page.dart
+++ lib/posts/post_detail_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:jobadda/model/post.dart';
 import 'package:jobadda/posts/post_dates.dart';
@@ -6,6 +8,7 @@ import 'package:jobadda/posts/post_info.dart';
 import 'package:jobadda/posts/post_links.dart';
 import 'package:jobadda/posts/post_vacancy_detail.dart';
 import 'package:jobadda/services/remote_api.dart';
+import 'package:share/share.dart';
 
 import '../colors.dart';
 
@@ -51,6 +54,49 @@ class PostDetailPage extends StatelessWidget {
               appBar: AppBar(
                 elevation: 0,
                 title: Text(post.title),
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.share),
+                    onPressed: () async {
+                      // Generate the dynamic link here for now
+                      // but use already created link get the link
+                      // through the post property (for faster sharing)
+                      final parameters = DynamicLinkParameters(
+                        uriPrefix: getUrlPrefix(),
+                        link: Uri.parse(
+                          "https://sarkarijobadda.in/${post.id}",
+                        ),
+                        androidParameters: AndroidParameters(
+                          packageName: getPackageName(),
+                          minimumVersion: 7,
+                        ),
+                        socialMetaTagParameters: SocialMetaTagParameters(
+                          title: post.title,
+                          description: post.info,
+                          imageUrl: Uri.parse(
+                            "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg",
+                          ),
+                        ),
+                      );
+
+                      final longUrl = await parameters.buildUrl();
+
+                      final shortenedLink =
+                          await DynamicLinkParameters.shortenUrl(
+                        longUrl,
+                        DynamicLinkParametersOptions(
+                          shortDynamicLinkPathLength:
+                              ShortDynamicLinkPathLength.short,
+                        ),
+                      );
+
+                      final url = shortenedLink.shortUrl;
+
+                      Share.share("${post.name} \n\nSee full post here:\n$url ",
+                          subject: post.title);
+                    },
+                  )
+                ],
               ),
               body: Column(
                 children: <Widget>[
@@ -89,5 +135,19 @@ class PostDetailPage extends StatelessWidget {
         return PostDetailShimmer();
       },
     );
+  }
+
+  String getUrlPrefix() {
+    if (kReleaseMode) {
+      return "https://jbda.page.link";
+    }
+    return "https://sjobadda.page.link";
+  }
+
+  String getPackageName() {
+    if (kReleaseMode) {
+      return "xyz.codingabc.jobadda";
+    }
+    return "xyz.codingabc.jobadda.debug";
   }
 }

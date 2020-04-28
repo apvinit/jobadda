73d48214a4463d58b9ac74312c6cd6cf934952d0
h4rdw1r3 <vinaugust6@gmail.com>
2020-04-28 15:29:33 +0530

    handle push notifications gracefully
    check if the value is passed before taking and decision

--- lib/services/push_messaging.dart
+++ lib/services/push_messaging.dart
@@ -1,29 +1,47 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:jobadda/posts/post_detail_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../main.dart';
-
 
 void configureNotification() {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   _firebaseMessaging.subscribeToTopic('users');
   _firebaseMessaging.configure(
     onMessage: (Map<String, dynamic> message) async {
-      print("onMessage: $message");
+      // called when app is in foreground
     },
     onBackgroundMessage: myBackgroundMessageHandler,
     onLaunch: (Map<String, dynamic> message) async {
-      print("onLaunch: $message");
+      // called when app is terminated
       final dynamic data = message['data'];
+
+      final String url = data["url"];
+      if (url != null && await canLaunch(url)) {
+        launch(url);
+        return;
+      }
+
       final String postId = data['postId'];
-      navigateToPostDetail(postId);
+      if (postId != null) {
+        navigateToPostDetail(postId);
+      }
     },
     onResume: (Map<String, dynamic> message) async {
-      print("onResume: $message");
+      // called when app is in background
       final dynamic data = message['data'];
+
+      final String url = data["url"];
+      if (url != null && await canLaunch(url)) {
+        launch(url);
+        return;
+      }
+
       final String postId = data['postId'];
-      navigateToPostDetail(postId);
+      if (postId != null) {
+        navigateToPostDetail(postId);
+      }
     },
   );
 }
@@ -39,10 +57,13 @@ Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
   print("onBackgroundMessage: $message");
   if (message.containsKey('data')) {
     // Handle data message
+
+    print("backgroundHandler : contains data");
   }
 
   if (message.containsKey('notification')) {
     // Handle notification message
+    print("backgroundHandler : contains notification");
   }
 
   // Or do other work.
